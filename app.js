13df0eb8933eff5df234f4b01bcf005a2c56ca4e
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-27 11:22:42 -0500

    JavaScript Variables

---
+++ app.js
@@ -0,0 +1,9 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = z;
+
+var price1 = 2;
+var price2 = 2;
+var total = price1 + price2;
+document.getElementById("price").innerHTML = "The total is: " + total;
